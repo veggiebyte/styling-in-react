a3322149a465f709d7cf0ad9abb41afb6eefd744
“CindyS” <veggiebyte@yahoo.com>
2026-01-09 15:33:47 -0800

    5 buttons

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Button1 from './components/example-1-normal-css/Button';
 import Button2 from './components/example-2-inline-css/Button';
 import Button3 from './components/example-3-styled-components/Button';
 import Button4 from './components/example-4-css-modules/Button';
+import Button5 from './components/example-5-sass/Button';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <Button2 buttonText="Example 2" />
       <Button3 buttonText="Example 3" />
       <Button4 buttonText="Example 4" />
+      <Button5 buttonText="Example 5" />
     </div>
   );
 }
--- src/components/example-5-sass/Button.jsx
+++ src/components/example-5-sass/Button.jsx
@@ -1,5 +1,8 @@
+import './button.sass';
+
 const Button = ({ buttonText }) => {
-  return <button>{buttonText}</button>;
+  // Using the class name from our Sass file:
+  return <button className="sassy-button">{buttonText}</button>; 
 };
 
 export default Button;
